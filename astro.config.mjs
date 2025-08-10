6ac2e7e4c2845e7c54d8ac49748ac5bf56da4c4c
Ismail <ismail.rintoprasetio@gmail.com>
2025-08-10 09:22:33 +0000

    test9

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,16 @@
 // // https://astro.build/config
 // export default defineConfig({});
 import { defineConfig } from 'astro/config';
+import fs from 'fs';
 // import github from '@astrojs/github';
 
 export default defineConfig({
   site: 'https://ismailrp.github.io',
   integrations: [],
 //   adapter: github(),
+  vite: {
+    buildEnd() {
+      fs.writeFileSync('./dist/.nojekyll', '');
+    }
+  }
 });
